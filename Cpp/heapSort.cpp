d98e8b5638963964feae498821af71fd70cf6fdf
YashMali <yashrajemali07@gmail.com>
2026-04-19 19:21:08 +0530

    publish

--- Cpp/heapSort.cpp
+++ Cpp/heapSort.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// ---------- MAX HEAP (for Ascending) ----------
 void maxHeapify(int arr[], int n, int i) {
     int largest = i;
     int left = 2 * i + 1;
@@ -20,18 +19,15 @@ void maxHeapify(int arr[], int n, int i) {
 }
 
 void heapSortAscending(int arr[], int n) {
-    // Build max heap
     for (int i = n / 2 - 1; i >= 0; i--)
         maxHeapify(arr, n, i);
 
-    // Extract elements
     for (int i = n - 1; i > 0; i--) {
         swap(arr[0], arr[i]);
         maxHeapify(arr, i, 0);
     }
 }
 
-// ---------- MIN HEAP (for Descending) ----------
 void minHeapify(int arr[], int n, int i) {
     int smallest = i;
     int left = 2 * i + 1;
@@ -50,18 +46,15 @@ void minHeapify(int arr[], int n, int i) {
 }
 
 void heapSortDescending(int arr[], int n) {
-    // Build min heap
     for (int i = n / 2 - 1; i >= 0; i--)
         minHeapify(arr, n, i);
 
-    // Extract elements
     for (int i = n - 1; i > 0; i--) {
         swap(arr[0], arr[i]);
         minHeapify(arr, i, 0);
     }
 }
 
-// ---------- MAIN ----------
 int main() {
     int n;
     cout << "Enter number of elements: ";
@@ -75,13 +68,11 @@ int main() {
         arr2[i] = arr[i]; // copy for descending
     }
 
-    // Ascending
     heapSortAscending(arr, n);
     cout << "\nAscending Order: ";
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
 
-    // Descending
     heapSortDescending(arr2, n);
     cout << "\nDescending Order: ";
     for (int i = 0; i < n; i++)
